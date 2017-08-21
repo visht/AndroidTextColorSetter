7aad9488db14f214b8c17b9462e96a39de57ccc5
Vishal Singh <vishal@qsstechnosoft.com>
2017-08-21 23:10:58 +0530

    updated code

---
+++ viewcolorlibrary/src/main/java/com/sdktextcolor/Util.java
@@ -0,0 +1,35 @@
+package com.sdktextcolor;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Utility class that includes all utility functions
+ */
+public class Util {
+
+    /**
+     * Setting view color
+     *
+     * @param view  view instance
+     * @param color color value
+     * @param ctx   activity context
+     */
+    @SuppressWarnings("deprecation")
+    public void setColor(View view, int color, Context ctx) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (view instanceof TextView)
+                ((TextView) view).setTextColor(ctx.getResources().getColor(color, null));
+            else
+                view.setBackgroundColor(ctx.getResources().getColor(color, null));
+        } else {
+            if (view instanceof TextView)
+                ((TextView) view).setTextColor(ctx.getResources().getColor(color));
+            else
+                view.setBackgroundColor(ctx.getResources().getColor(color));
+        }
+    }
+
+}
